1863ccf100c4f5804e5b0aa2b63acb1643f9fb91
lunarpeak77 <lunarpeak77@gmail.com>
2025-04-03 08:53:16 +0900

    Add development log

---
+++ src/nova5rush5pro-bot_log.py
@@ -0,0 +1,28 @@
+# Development Log
+# Started at 2025-04-03 08:53:16
+
+# Added some random functionality
+# Fixed some bugs
+import sqlite3
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+        return None
+
+def create_table(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT UNIQUE
+            )
+        ''')
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
