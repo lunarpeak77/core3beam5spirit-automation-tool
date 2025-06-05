fbc45afe47820dca9b220e9e042b422171d85e36
lunarpeak77 <lunarpeak77@gmail.com>
2025-06-05 21:32:08 +0900

    Update development log at 2025-06-05 21:32:07

--- src/nova5rush5pro-bot_log.py
+++ src/nova5rush5pro-bot_log.py
@@ -1024,3 +1024,17 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n-1)
+
+# Update at 2025-06-05 21:32:07
+# Fixed some bugs
+
+
+import json
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def load_from_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
