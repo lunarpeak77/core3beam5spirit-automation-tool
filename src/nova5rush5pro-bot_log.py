3f81fbb83b72d0864c5b038e8f78d5fb364cd078
lunarpeak77 <lunarpeak77@gmail.com>
2025-06-11 05:22:11 +0900

    Update development log at 2025-06-11 05:22:10

--- src/nova5rush5pro-bot_log.py
+++ src/nova5rush5pro-bot_log.py
@@ -1126,3 +1126,17 @@ def resize_image(image_path, size):
 def convert_to_grayscale(image_path):
     with Image.open(image_path) as img:
         return img.convert('L')
+
+# Update at 2025-06-11 05:22:10
+# Added unit tests
+# Updated the code with new features
+from PIL import Image
+
+def resize_image(image_path, size):
+    with Image.open(image_path) as img:
+        resized = img.resize(size)
+        return resized
+
+def convert_to_grayscale(image_path):
+    with Image.open(image_path) as img:
+        return img.convert('L')
