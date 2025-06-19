8c21971cf6541a6c1890e1c3e10a5aa16e105101
lunarpeak77 <lunarpeak77@gmail.com>
2025-06-19 22:49:04 +0900

    Update development log at 2025-06-19 22:49:04

--- src/nova5rush5pro-bot_log.py
+++ src/nova5rush5pro-bot_log.py
@@ -1211,3 +1211,13 @@ def read_file_lines(filename):
 def write_file_lines(filename, lines):
     with open(filename, 'w') as file:
         file.writelines(lines)
+
+# Update at 2025-06-19 22:49:04
+# Fixed some bugs
+
+def count_words(text):
+    words = text.split()
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    return word_count
