4ef836e7fb64f007588bcef474a0ded8576de031
lunarpeak77 <lunarpeak77@gmail.com>
2025-10-14 00:24:11 +0900

    Update development log at 2025-10-14 00:24:08

--- src/nova5rush5pro-bot_log.py
+++ src/nova5rush5pro-bot_log.py
@@ -1386,3 +1386,18 @@ def read_file_lines(filename):
 def write_file_lines(filename, lines):
     with open(filename, 'w') as file:
         file.writelines(lines)
+
+# Update at 2025-10-14 00:24:08
+# This is a random comment
+
+
+from PIL import Image
+
+def resize_image(image_path, size):
+    with Image.open(image_path) as img:
+        resized = img.resize(size)
+        return resized
+
+def convert_to_grayscale(image_path):
+    with Image.open(image_path) as img:
+        return img.convert('L')
